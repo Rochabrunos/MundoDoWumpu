d30be38af60cf3a8b38df5b74d82a30c0c527421
Bruno <brunos-rocha@hotmail.com>
2017-11-11 14:10:15 -0200

    Testing

--- MundoDoWumpus/src/game/Inteface.java
+++ MundoDoWumpus/src/game/Inteface.java
@@ -7,7 +7,7 @@ public class Inteface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Agente myAgent = new Agente(0, 0);
+		Agente myFuckingAgent = new Agente(0, 0);
 	}
 
 }
